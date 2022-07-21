f44acf4180854b0b263402d371e9b9d5865ccfc5
Simone Carriero <simonecarriero95@hotmail.com>
2022-07-22 01:30:54 +0200

    Setted pwa features

--- src/index.js
+++ src/index.js
@@ -26,6 +26,18 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+
+serviceWorkerRegistration.register({
+  //Features to make the page reload automatically in case of changes to the project
+  onUpdate: async (registration) => {
+    
+    if (registration && registration.waiting) {
+      await registration.unregister();
+      registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      
+      window.location.reload();
+    }
+  },
+});
 
 
